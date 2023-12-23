e4a9644bd44092a14d48df6588cb1e1cdf1c0d11
201907310张凯德 <1871512203@qq.com>
2023-12-23 17:58:14 +0800

    测试

---
+++ ff14LogsBot/src/main/java/com/own/ff14logsbot/Ff14LogsBotApplication.java
@@ -0,0 +1,13 @@
+package com.own.ff14logsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ff14LogsBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ff14LogsBotApplication.class, args);
+    }
+
+}
---
+++ ff14LogsBot/src/test/java/com/own/ff14logsbot/Ff14LogsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.own.ff14logsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ff14LogsBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
